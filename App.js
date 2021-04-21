2ae511032fbf873d043468b74e7ddb1afcd58d1e
Danila Nikonec <danila.nikonets@gmail.com>
2021-04-21 13:24:33 +0500

    ReactNativeConfig install

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from "react-native-config";
 
 export default function App() {
+  console.log('Config=> ', Config)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{Config.BUILD_TYPE}</Text>
       <StatusBar style="auto" />
     </View>
   );
